46f47be05d6024829f2d1eb8403538ba23b8c7a1
Omri Zuta <omrizu@mta.ac.il>
2020-07-08 21:12:00 +0300

    Add project files.

---
+++ Algorithms project/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+// This program gets text file with graph and the graph data.
+// the program will make a Flow network
+// the maximum flow of the Flow network will be printed,
+// and also, its minimal cut.
+// the program will use 2 different searching methods:
+//BFS and with a greedy method
+
+void main(int argc, char* argv[])
+{
+
+
+
+}
