6bacec33e6e2959949ea571b1d28ad8774b30fe4
Omri Zuta <omrizu@mta.ac.il>
2020-07-09 20:56:15 +0300

    first update

--- Algorithms project/main.cpp
+++ Algorithms project/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
+#include <string>
 using namespace std;
 
 // This program gets text file with graph and the graph data.
@@ -9,8 +11,18 @@ using namespace std;
 // the program will use 2 different searching methods:
 //BFS and with a greedy method
 
+// argc count how much arguments have been sent to main
+// argv is the arguments itself, represented as string
+// it's comfortable to debug this program with cmd : Algorithms_project input.txt
 void main(int argc, char* argv[])
 {
+     int numOfVertex, numOfAdjes, s, t;
+     ifstream inFile(argv[1]); // opening file called "input.txt"
+     inFile >> numOfVertex >> numOfAdjes>> s>> t;  //init those variables
+
+     cout <<"num Of Vertex is: " <<numOfVertex << " num Of Adjes is: " 
+          << numOfAdjes << " s Vertex is: " <<s << " t  Vertex is: " <<t;
+     inFile.close();
 
 
 
