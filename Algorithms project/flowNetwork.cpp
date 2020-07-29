1e8c3a27209227b715190497b564b505c2002a45
Niv Gitzi <nivgi@mta.ac.il>
2020-07-29 22:31:20 +0300

    added notes 29/7 in flownetwork

--- Algorithms project/flowNetwork.cpp
+++ Algorithms project/flowNetwork.cpp
@@ -225,9 +225,9 @@ int FlowNetwork::fordFulkersonMethodWithDijskstraVaration()
 
 int* FlowNetwork::DijskstraVarationMethod()
 {
-     // creating array in order to create maxHeap
-     int* pArray = new int[m_graph.getNumOfVertexes()];
-     int* dArray = new int[m_graph.getNumOfVertexes()];
+     // creating array in order to create maxHeap 
+     int* pArray = new int[m_graph.getNumOfVertexes()];               //niv 29/7 note: we need to free the array???
+     int* dArray = new int[m_graph.getNumOfVertexes()];               //niv 29/7 note: we need to free the array???
      HeapElement* heapElementsArray = new HeapElement[m_graph.getNumOfVertexes()];
      initialzeSingleSource(heapElementsArray, pArray, dArray);
      MaxHeap maxHeap(heapElementsArray,m_graph.getNumOfVertexes());
@@ -250,9 +250,9 @@ int* FlowNetwork::DijskstraVarationMethod()
      //delete later:
      for (int i = 0; i < m_graph.getNumOfVertexes(); i++)
      {
-          cout << " " << pArray[i]+1;
-
+          cout << " " << pArray[i]+1;		  
      }
+	 cout << endl; //change niv 29.7 for after ptinting p array, looks better
 
      return pArray; 
 }
@@ -267,14 +267,15 @@ void FlowNetwork::initialzeSingleSource(HeapElement* i_ElementHeapArray, int* i_
           i_pArray[i] = NO_PARENT;
      }
      i_ElementHeapArray[m_graph.getSvertex()].setkey(0);// maybe because we use max heap we need to make it something else. delete later
+	 i_dArray[m_graph.getSvertex()] = 0;     //change niv 29.7         added this, it seems we need it
 }
 
 void FlowNetwork::relax(int i_uVertex, int i_vVertex , int* i_pArray, MaxHeap & i_maxHeap, int * i_dArray)
 {
-     int edgeUvCF = m_graph.getEdgeCf(i_uVertex, i_vVertex);
+     int edgeUvCF = m_graph.getEdgeCf(i_uVertex, i_vVertex); 
      int pathThroughUCF = min(i_dArray[i_uVertex], edgeUvCF);
      // checks if the path to v vertex has a no cf 
-     if (i_dArray[i_vVertex] == INFINITY_VAL)
+     /*if (i_dArray[i_vVertex] == INFINITY_VAL)
      {
           if (i_uVertex == m_graph.getSvertex())
           {
@@ -290,9 +291,9 @@ void FlowNetwork::relax(int i_uVertex, int i_vVertex , int* i_pArray, MaxHeap & 
         //increase key
           i_maxHeap.IncreaseKey(i_vVertex, i_dArray[i_vVertex]);
 
-     }
+     }*/
      //or less cf the if it uses u vertex
-     else if (pathThroughUCF > i_dArray[i_vVertex])
+     /*else*/ if (pathThroughUCF > i_dArray[i_vVertex]) //            change 29.7 niv:          seems we dont need the above "if" at all! still need to check
      {
           i_dArray[i_vVertex]=pathThroughUCF;
           i_pArray[i_vVertex] = i_uVertex;
